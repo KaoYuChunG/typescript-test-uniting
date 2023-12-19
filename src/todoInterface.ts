abc021589087e8245d86506e35cfe2206f86fc9e
Kao <kaoyuchun14@gmail.com>
2023-12-19 01:04:07 -0300

    test completed

---
+++ src/todoInterface.ts
@@ -0,0 +1,29 @@
+/**
+ * 待辦事項介面已被移除，你需要自己定義一個新的待辦事項介面。
+ * 待辦事項介面應該包含 id、task 和 completed 三個屬性，分別為數字型別、字串型別和布林型別。
+ */
+
+/**
+ * 新增待辦事項
+ * @param todos - 現有的待辦事項陣列
+ * @param task - 新的待辦事項的內容
+ * 
+ * 範例:
+ * 輸入: ([{ id: 1, task: 'Buy milk', completed: false }], 'Walk the dog')
+ * 輸出: [{ id: 1, task: 'Buy milk', completed: false }, { id: 2, task: 'Walk the dog', completed: false }]
+ */
+
+export function addTodo(todos: Todo[], task: string): Todo[] {
+    const newId = todos.length + 1;
+    const newTodo = {
+        id: newId,
+        task: task,
+        completed: false,
+    };
+    return [...todos, newTodo];
+}
+interface Todo {
+    id: number;
+    task: string;
+    completed: boolean;
+}
